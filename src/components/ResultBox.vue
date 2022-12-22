268469189f23ef62568c3036c2c887414608dcad
simonlee0109 <60864932+simonlee0109@users.noreply.github.com>
2022-12-22 15:23:30 +0800

    version 2

--- src/components/ResultBox.vue
+++ src/components/ResultBox.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-        <div class="border-solid border-l-2 border-t-2 border-r-8 border-b-8 border-black bg-white mr-2 mb-2 grid place-items-center aspect-square"  style="border-radius: 35px;">
-            <div class="text-center text-[56px] font-extrabold" >{{ value.winner }}</div>
-            <div class="absolute top-[10px] left-[28px] text-[28px] font-extrabold">{{ value.result_no }}</div>
+    <div class="h-[100%] pt-[10px] ">
+        <div class="border-solid border-l-2 border-t-2 border-r-8 border-b-8 border-black bg-white mr-2 mb-2 grid place-items-center h-[100%] "  style="border-radius: 35px;">
+            <div class="text-center text-[4vw] font-extrabold" >{{ value.winner }}</div>
+            <div class="absolute top-[5%] left-[5%] text-[2vw] font-extrabold">{{ value.result_no }}</div>
         </div>
     </div>
 </template>
@@ -11,7 +11,7 @@
 
 export default({
     props:{
-        value:String
+        value:Object
     }
 })
 </script>
--- src/components/ResultPage.vue
+++ src/components/ResultPage.vue
@@ -1,76 +1,86 @@
 <template>
-    <div>
-        <div  class="text-center text-kadenYellow font-sunny description text-[100px]">Konew Annual Dinner Lucky Draw</div>
-        <div class="grid grid-cols-2 md:grid-cols-6 gap-4">
-            <div v-for="item in list" :key="item.result_no">
+<div>
+    <div class="flex flex-col w-[100%] h-[100vh] relative">
+        <div class="btn absolute top-10 left-10" @click="now_page-=1" v-if="now_page>=1">prev page</div>
+        <div class="btn absolute top-10 right-10" @click="now_page+=1" v-if="now_page<(this.list.length/30)-1">next page</div>
+        <div class="text-center text-kadenYellow description text-[5vw]" style="pointer-events:none;">Konew Annual Dinner Lucky Draw</div>
+        <div class="flex mb-10 mr-10 ml-10 flex-wrap">
+            <div v-for="item in display_list[now_page]" :key="item.result_no" class="w-[16%] ">
                 <ResultBox :value="item"></ResultBox>
             </div>
         </div>
     </div>
+
+</div>
 </template>
 
 <script>
 import ResultBox from './ResultBox.vue'
 import db from '../firebase/init'
-import { collection, getDoc, getDocs, onSnapshot, orderBy, query } from '@firebase/firestore';
+import {
+    collection,
+    getDoc,
+    getDocs,
+    onSnapshot,
+    orderBy,
+    query
+} from '@firebase/firestore';
 export default ({
-    data(){
-        return{
-            list:[
-            ],
+    data() {
+        return {
+            list: [],
+            display_list:[],
+            now_page: 0,
         }
     },
-    components:{
+    components: {
         ResultBox
     },
-    created(){
-         this.readEmployees();
+    created() {
+        this.readEmployees();
     },
-    methods:{
-        async readEmployees() {
-            onSnapshot(query(collection(db, 'results'), orderBy('created_at')),  (snapshot) => {
-                this.list = []
-                          console.log(snapshot);
-                          snapshot.forEach((doc) => {
-                    
-                this.list.push(doc.data())
-            })
-             },
-  (error) => { });
-            // let sp = await getDocs(ref);
-            // console.log(sp);
-            // sp.forEach((doc) => {
-            //     this.list.push(doc.data())
-            // })
-    //   db.collection("results")
-    //     .get()
-    //     .then((querySnapshot) => {
-    //       querySnapshot.forEach((doc) => {
-    //         this.list.push({
-    //           id: doc.id,
-    //           result_no: doc.data().result_no,
-    //           winner: doc.data().winner,
-    //         });
-    //         console.log(doc.id, " => ", doc.data());
-    //       });
-    //     })
-    //     .catch((error) => {
-    //       console.log("Error getting documents: ", error);
-    //     });
-    }
+    methods: {
+        readEmployees() {
+            onSnapshot(query(collection(db, 'results'), orderBy('created_at')), (snapshot) => {
+                    this.list_length = snapshot.docs.length
+                    this.list = []
+                    snapshot.forEach((doc) => {
+                        this.list.push(doc.data())
+                    })
+                    this.cut_array();
+                },
+                (error) => {}
+            )
+        },
+        cut_array(){
+            this.display_list = []
+            for(let i = 0;i<this.list.length/30;i++){
+                let array = this.list.slice(30*i,30*(i+1))
+                this.display_list.push(array);
+            }
+            console.log(this.display_list);
+        },
     }
 })
-</script>
-<style>
- .description{
-     font-family: "Sunny Spells", Helvetica, Arial;
-     text-shadow: 6px 7px 0px black;
-  }
+</script> 
 
-  @font-face {
-  font-family: "Sunny Spells";
-  src: local("Sunny Spells"),
-   url(@/assets/fonts/sunny_spells.ttf) format("truetype");
+<style>
+.description {
+    font-family: "Sunny Spells", Helvetica, Arial;
+    text-shadow: 6px 7px 0px black;
 }
 
+@font-face {
+    font-family: "Sunny Spells";
+    src: local("Sunny Spells"),
+        url(@/assets/fonts/sunny_spells.ttf) format("truetype");
+}
+
+.btn{
+    border: 2px black solid;
+    border-radius: 15px;
+    background-color: red;
+    font-size:26px;
+    padding: 10px;
+}
 </style>
